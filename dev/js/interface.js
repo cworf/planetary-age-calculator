de0c0ec26e2eee4024fd4464aa775d7444803393
cworf <colin.worf@gmail.com>
2018-01-05 15:54:05 -0800

    add basic front end logic

--- dev/js/interface.js
+++ dev/js/interface.js
@@ -1,3 +1,15 @@
 $(function(){
-	//front end stuff
+	$( "#birthday" ).datepicker({
+		changeMonth: true,
+		changeYear: true
+    });
+	$('#user-input').submit(function(event){
+		const birthday = parseInt($('#birthday').val()),
+			place = $('#place').val();
+		if (birthday && place) {
+			const person = new Person(birthday, place);
+		} else {
+			alert('please fill out all fields');
+		}
+	});
 });
