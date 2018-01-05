2011cf9e2a013465678750d3efcb9f533aba387f
cworf <colin.worf@gmail.com>
2018-01-05 14:45:27 -0800

    write logic for calculating life expectancy

--- dev/js/logic.js
+++ dev/js/logic.js
@@ -1,11 +1,13 @@
 class Person {
-	constructor(birthDay){
-		this.earthSeconds = this.ageSeconds(birthDay); //birthday enters constructor as ms from epoch
+	constructor(day, place){
+		this.birthDay = day;
+		this.earthSeconds = this.ageSeconds(day); //birthday enters constructor as ms from epoch
 		this.earthYears = this.yearRnd();
 		this.mercuryYears = Math.round(10 * (this.earthYears / .24)) / 10;
 		this.venusYears = Math.round(10 * (this.earthYears / .62)) / 10;
 		this.marsYears = Math.round(10 * (this.earthYears / 1.88)) / 10;
 		this.jupiterYears = Math.round(10 * (this.earthYears / 11.86)) / 10;
+		this.lifeExpectancy = lifeExp(day, place)
 	}
 
 	ageSeconds(bDay){
@@ -17,6 +19,19 @@ class Person {
 	yearRnd(){
 		const years = this.earthSeconds / 31556926;
 		return Math.round(10*years)/10;
+	}
+
+	lifeExp(day, place){
+		const now = 2018;
+		const birthYear = day / 31556926000 + 1970;
+		const worldAverageNow = 70;
+		const locationMod = {
+			"United States" : 10,
+			"Latin America" : 4,
+			"Asia" : -1,
+			"Africa" : -5
+		};
+		return worldAverageNow + (birthYear - now) * .24 + locationMod[place];
 	}
 }
 
