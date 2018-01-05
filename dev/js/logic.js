35bb3c93031a395e5f106ee0cac43d14d8a6498b
cworf <colin.worf@gmail.com>
2018-01-05 12:05:48 -0800

    write code for age calc and test

--- dev/js/logic.js
+++ dev/js/logic.js
@@ -1,6 +1,22 @@
 class Person {
-	constructor(age){
-		this.earthYears = age;
+	constructor(birthDay){
+		this.earthSeconds = this.ageSeconds(birthDay); //birthday enters constructor as ms from epoch
+		this.earthYears = this.yearRnd();
+		this.mercuryYears = this.earthYears / .24;
+		this.venusYears = this.earthYears / .62;
+		this.marsYears = this.earthYears / 1.88;
+		this.jupiterYears = this.earthYears / 11.86;
+	}
+
+	ageSeconds(bDay){
+		const timeNow = Date.now(); //get current date in ms from epoch
+		const ageSeconds = (timeNow - bDay) / 1000;
+		return ageSeconds;
+	}
+
+	yearRnd(){
+		const years = this.earthSeconds / 31556926;
+		return Math.round(10*years)/10;
 	}
 }
 
--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,12 +1,15 @@
 const Person = require('./../dev/js/logic.js').personModule;
 
 describe( 'Person', function(){
-	it('should calculate users age in earth years', function(){
-		const person = new Person(35);
-		expect(person.earthYears).toEqual(25);
+	it('should convert users age into earth years', function(){
+		const person = new Person(575494240000);
+		// expect(person.earthYears > 29 && person.earthYears < 30).toBeTruthy(); //will be true until Im 30
+		expect(person.earthYears).toEqual(29.8); //this will be true for a short while
 	});
-	it('should convert age into minutes', function(){
-
+	it('should calculate users age in seconds', function(){
+		const person = new Person(575494240000),  //575494240000 will be true (is my birthday of march 27th 1988 from time of writing this)
+			earthSec = person.earthSeconds;
+		expect(earthSec > 938521743 && earthSec < 940336128).toBeTruthy(); //this is a range of about + or - 10 days in seconds from my current age
 	});
 	it('should convert earth years into Mercury years', function(){
 
