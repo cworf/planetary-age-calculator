d9c6984cf27fb475be8a2b71699e096492497912
cworf <colin.worf@gmail.com>
2018-01-05 16:22:08 -0800

    make dist folder clean before anything

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+	del = require('del');
 
-gulp.task('copyStuff', function(){
+
+
+gulp.task('clean', function(){
+	del(['tmp', 'dist']);
+})
+
+gulp.task('copyStuff', ['clean'], function(){
 	gulp.src('dev/index.html')
 		.pipe(gulp.dest('./dist'));
 	gulp.src('dev/css/master.css')
