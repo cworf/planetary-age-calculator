b4bf789d935141383fd8a9af54dac1fc4a8719c4
cworf <colin.worf@gmail.com>
2018-01-05 16:56:31 -0800

    add jquery and bootstrap to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,42 @@
 const gulp = require('gulp'),
-	del = require('del');
-
+	del = require('del'),
+	lib = require('bower-files') ({
+		  "overrides":{
+		    "bootstrap" : {
+		      "main": [
+		        "less/bootstrap.less",
+		        "dist/css/bootstrap.css",
+		        "dist/js/bootstrap.js"
+		      ]
+		    }
+		  }
+	  }),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
 
 
 gulp.task('clean', function(){
 	del(['tmp', 'dist']);
-})
+});
 
-gulp.task('copyStuff', ['clean'], function(){
-	gulp.src('dev/index.html')
-		.pipe(gulp.dest('./dist'));
+gulp.task('copyHTML', ['clean'], function(){
 	gulp.src('dev/css/master.css')
 		.pipe(gulp.dest('./dist/css'));
-})
+});
+gulp.task('copyCSS', ['copyHTML'], function(){
+	gulp.src('dev/css/master.css')
+		.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('bowerCSS', ['copyCSS'], function () {
+	gulp.src(lib.ext('css').files) //grab css
+		.pipe(concat('vendor.css'))
+		.pipe(gulp.dest('./dist/css'));
+});
+
+// gulp.task('bowerJS', ['bowerCSS'], function(){
+// 	gulp.src(lib.ext('js').files) //grab js
+// 	.pipe(concat('vendor.min.js'))
+// 	.pipe(uglify())
+// 	.pipe(gulp.dest('./dist/js'));
+// });
